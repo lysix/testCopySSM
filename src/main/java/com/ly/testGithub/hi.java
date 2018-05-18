4cd6f9de7cb3a2954a1dbc850d64c241ae41be0d
lysix <2772054765@qq.com>
2018-05-18 14:45:24 +0800

    add 1

--- src/main/java/com/ly/testGithub/hi.java
+++ src/main/java/com/ly/testGithub/hi.java
@@ -4,6 +4,9 @@ public class hi {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
         System.out.println("hello git");
+
+        System.out.println("hello shell");
     }
 }
